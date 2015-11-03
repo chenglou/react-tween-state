0786e442058fca294f8a57b5dfbb7c0d2df694aa
TN1ck <tomwanick@gmail.com>
2015-11-03 11:18:17 +0100

    updated to react 0.14.2

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,6 +1,7 @@
 var App1 = require('./example1.jsx');
 var App2 = require('./example2.jsx');
 var React = require('react');
+var ReactDOM = require('react-dom');
 
-React.render(<App1 />, document.querySelector('#content1'));
-React.render(<App2 />, document.querySelector('#content2'));
+ReactDOM.render(<App1 />, document.querySelector('#content1'));
+ReactDOM.render(<App2 />, document.querySelector('#content2'));
